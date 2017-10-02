3f72f1ac5c4f45968f547d011b8ebcf4dea24f7f
vangali12 <30483940+vangali12@users.noreply.github.com>
2017-10-02 10:39:38 -0700

    02Oct2017 commit

--- 02Oct2017_numGirl.js
+++ 02Oct2017_numGirl.js
@@ -1,17 +1,19 @@
 // build a function that takes 2 parameters. If the number is less than 5, return "low number guuuurl". If it is greater than 5, return "big number". If it equals five then return "5".
 
-// function numGirlSwitch(x) {
-// 	switch(x) {
-// 		case <5:
-// 			return "low number guuuurl";
-// 		case >5:
-// 			return "big number";
-// 		case ==5:
-// 			return "5";
-// 	}
-// }
+function numGirlSwitch(x) {
+	switch(true) {
+		case (x<5):
+			return "low number guuuurl";
+		case (x>5):
+			return "big number";
+		case (x==5):
+			return "5";
+	}
+}
  
 // The switch case should only be used if the value being considered is BEING COMPARED TO A SPECIFIC VALUE, not a condition. In this case, the switch case should not be used becuase we are not comparing integers to integers.
+
+// If you want to use the switch case for conditional statements, you pass in the value "true" and each of the conditional statements will test against the value "true". If the condition is true, the action will happen. It not, it will pass on to the next condition.
 
 
 function numGirlIf(x) {
@@ -26,6 +28,8 @@ function numGirlIf(x) {
 	}
 }
 
-console.log(numGirlIf(7));
-console.log(numGirlIf(0));
-console.log(numGirlIf(5));
+
+console.log(numGirlSwitch(12));
+// console.log(numGirlIf(7));
+// console.log(numGirlIf(0));
+// console.log(numGirlIf(5));
---
+++ 02Oct2017_subVals.js
@@ -0,0 +1,5 @@
+// write a function that returns the difference between two values. Extra challenge, subtract the smaller number from the larger number.
+
+function sub(x, y) {
+	return Math.abs(x - y);
+}
